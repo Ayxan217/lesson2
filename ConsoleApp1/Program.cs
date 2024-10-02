9d796ced8a58ec828d2bf7f81da48292c8c15cd4
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-02 19:14:11 +0400

    lesson 2 added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+//int end = 5;
+//int factorial = 1;
+//for (int i = 1; i <= end; i++)
+//{
+
+//    factorial *= i;
+
+//}
+//Console.WriteLine(factorial);
+//int end = 10;
+//int num1 = 0;
+//int num2 = 1;
+//int temp;
+//for (int i = 0; i < end - 2; i++)
+//{
+//    temp = num1;
+//    num1 = num2;
+//    num2 = temp + num2;
+
+//}
+//Console.WriteLine(num2);
+
+//float sum = 0;
+//float count = 0;
+//float result = 0;
+
+//for (int i = 1; i <= 100; i++)
+//{
+//    if (i % 15 == 0)
+//    {
+//        count++;
+//        sum = sum + i;
+//        result = sum / count;
+
+
+
+//    }
+//}
+//Console.WriteLine(result);
